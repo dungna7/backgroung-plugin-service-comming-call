f3b857dccde87f6850abb3b79d202f02c535c03c
phunm128 <phunm128@gmail.com>
2018-10-11 16:54:18 +0900

    update service run receive comming call after 5 minutes

--- src/android/service.java
+++ src/android/service.java
@@ -1,5 +1,6 @@
 package com.service.backgroundcall;
 
+import android.os.Handler;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -29,6 +30,8 @@ public class service extends Service {
 
     private Socket mSocket;
     private static final String URL = "http://153.127.242.114:3000";
+    private static final String socketEmit = "voicechat:before_call";
+    private static final String socketListen = "voicechat:receiveCall";
 
     public service() {
     }
@@ -46,43 +49,49 @@ public class service extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String socketURL = URL;
-        String socketEmit = "voicechat:before_call";
-        String socketListen = "voicechat:receiveCall";
-        try {
-            mSocket = IO.socket(socketURL);
-            mSocket.connect();
-            mSocket.emit(socketEmit);
-            mSocket.on(socketListen, new Emitter.Listener() {
-                @Override
-                public void call(Object... args) {
-                    JSONObject data = (JSONObject) args[0];
-                    // Check if the Device is Locked Or Not
-                    KeyguardManager myKM = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
-                    boolean isPhoneLocked = myKM.inKeyguardRestrictedInputMode();
-                    // Awake Device
-                    PowerManager powerManager = ((PowerManager) getSystemService(Context.POWER_SERVICE));
-                    PowerManager.WakeLock wakeLock = powerManager
-                            .newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
-                    int level = PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP;
-                    wakeLock = powerManager.newWakeLock(level, "TAG");
-                    wakeLock.setReferenceCounted(false);
-                    wakeLock.acquire();
-                    // start app
-                    Intent dialogIntent = new Intent(service.this,
-                            nisshin.ComeEchat.PrototypeVersion.MainActivity.class);
-                    dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
-                            | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                    dialogIntent.putExtra("serviceCallInfo", data.toString());
-                    dialogIntent.putExtra("serviceScrennInfo", isPhoneLocked);
-                    startActivity(dialogIntent);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mSocket = IO.socket(URL);
+                    Log.d("RECEIVER_serviceCall", "beforce call 10 s");
+                    mSocket.connect();
+                    mSocket.emit(socketEmit);
+                    Log.d("RECEIVER_serviceCall", "after call 10 s");
+                    mSocket.on(socketListen, new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            JSONObject data = (JSONObject) args[0];
+                            // Check if the Device is Locked Or Not
+                            KeyguardManager myKM = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+                            boolean isPhoneLocked = myKM.inKeyguardRestrictedInputMode();
+                            // Awake Device
+                            PowerManager powerManager = ((PowerManager) getSystemService(Context.POWER_SERVICE));
+                            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(
+                                    PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+                            int level = PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP;
+                            wakeLock = powerManager.newWakeLock(level, "TAG");
+                            wakeLock.setReferenceCounted(false);
+                            wakeLock.acquire();
+                            // start app
+                            Intent dialogIntent = new Intent(service.this,
+                                    nisshin.ComeEchat.PrototypeVersion.MainActivity.class);
+                            dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+                                    | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            dialogIntent.putExtra("serviceCallInfo", data.toString());
+                            dialogIntent.putExtra("serviceScrennInfo", isPhoneLocked);
+                            startActivity(dialogIntent);
+                        }
+                    });
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
                 }
-            });
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        return super.onStartCommand(intent, flags, startId);
+            }
+        }, 10000);
+        // If we get killed, after returning from here, restart
+        return START_STICKY;
+        // return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
